0d1cf51cd9e0fd8d2718533a38178e60735c88d1
Swiichy <adlpromail@gmail.com>
2023-01-15 23:58:44 +0100

    feat: integration on side filter

--- src/components/common/ButtonFilter/index.jsx
+++ src/components/common/ButtonFilter/index.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-export const ButtonFilter = ({ item }) => {
+export const ButtonFilter = ({ item, handleClick }) => {
   const { text, active } = item;
 
-  return <Container active={active}>{text}</Container>;
+  return (
+    <Container active={active} onClick={() => handleClick(text)}>
+      {text}
+    </Container>
+  );
 };
 
 export const Container = styled.div`
@@ -12,16 +16,17 @@ export const Container = styled.div`
   font-weight: 600;
   font-size: 13px;
   line-height: 19px;
-  color: #4661e6;
+  color: ${({ active }) => (active ? "#FFFFFF" : "#4661e6")};
   text-transform: capitalize;
-  background: ${({ active }) => (active ? "red" : "#f2f4ff")};
+  background: ${({ active }) => (active ? "#4661E6" : "#f2f4ff")};
   border-radius: 10px;
   padding: 5px 17px 6px 16px;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
 
   &:hover {
-    color: #4661e6;
+    color: ${({ active }) => (active ? "#FFFFFF" : "#4661e6")};
     background: #cfd7ff;
+    background: ${({ active }) => (active ? "#4661E6" : "#cfd7ff")};
   }
 `;
--- src/components/view/AsideFilter/index.jsx
+++ src/components/view/AsideFilter/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { AsideCardLayout } from "../../layout/AsideCardLayout";
 import { ButtonFilter } from "../../common/ButtonFilter";
 
 export const AsideFilter = () => {
-  const items = [
+  const initialValue = [
     { text: "All", active: false },
     { text: "UI", active: false },
     { text: "UX", active: false },
@@ -12,11 +12,24 @@ export const AsideFilter = () => {
     { text: "Bug", active: false },
     { text: "Feature", active: false },
   ];
+
+  const [items, setItems] = useState(initialValue);
+
+  const handleClick = (text) => {
+    const newItems = items.map((item) => {
+      if (item.text === text) {
+        return { ...item, active: !item.active };
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+
   return (
     <AsideCardLayout>
       <Container>
         {items.map((item) => {
-          return <ButtonFilter item={item} />;
+          return <ButtonFilter item={item} handleClick={handleClick} />;
         })}
       </Container>
     </AsideCardLayout>
